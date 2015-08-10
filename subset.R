323e63ce744bfae16ace17d2de0f9178ea24f2c5
josiahdavis <josiah.j.davis@gmail.com>
2015-08-10 07:35:24 -0700

    Initial commit

---
+++ subset.R
@@ -0,0 +1,20 @@
+# Create subset of yelp data for analysis
+
+loc <- '/Users/josiahdavis/Documents/DraftBlog/yelp_dataset_challenge_academic_dataset/'
+
+# Read in business attributes
+db <- read.csv(paste(loc, 'yelp_academic_dataset_business.csv', sep=""))
+
+# Subset to only include Verizon Wireless business information
+db <- db[grepl("Verizon Wireless", db$name),]
+
+# Read in review text
+dr <- read.csv(paste(loc, 'yelp_academic_dataset_review.csv', sep=""), sep=",")
+
+# Subset to only include those for Verizon Wireless
+dr <- dr[dr$business_id %in% db$business_id, ]
+
+# Write subset data
+newLoc <- '/Users/josiahdavis/Documents/GitHub/Customer/'
+write.csv(db, paste(newLoc, 'yelp_business.csv', sep=""))
+write.csv(dr, paste(newLoc, 'yelp_review.csv', sep=""))
---
+++ wordCounts.R
@@ -0,0 +1,64 @@
+# ANALYSIS OF YELP DATA
+
+# Read in data
+loc <- '/Users/josiahdavis/Documents/GitHub/Customer/'
+db <- read.csv(paste(loc, 'yelp_business.csv', sep=""))
+dr <- read.csv(paste(loc, 'yelp_review.csv', sep=""))
+
+# -----------------------------------
+# Look up the most frequent terms
+
+# Convert the relevant data into a corpus object with the tm package
+require(tm)
+d <- Corpus(VectorSource(dr$text))
+
+# Convert everything to lower case
+d <- tm_map(d, content_transformer(tolower))
+
+# Remove numbers
+d <- tm_map(d, removeNumbers)
+
+# Remove punctuation
+d <- tm_map(d, removePunctuation)
+
+# Perform specific transfomrations (Good way to handle misspellings and acronyms)
+transformString <- content_transformer(function(x, from, to) gsub(from, to, x))
+d <- tm_map(d, transformString, "servcie", "service")
+
+# Remove stopwords (generic)
+d <- tm_map(d, removeWords, stopwords("english"))
+
+# Remove stopwords (specific to context)
+sw <- c("verizon", "wireless", "phone", "store", "service", "customer", "help", 
+        "get", "time", "back", "didnt", "walk", "great")
+d <- tm_map(d, removeWords, sw)
+
+# Stem the stopwords using a snowball stemmer
+#tm_map(Corpus(VectorSource("clothe clothes clothed clothing")), stemDocument)[[1]]$content #example
+#d <- tm_map(d, stemDocument)
+
+
+# Strip whitespace
+d <- tm_map(d, stripWhitespace)
+
+# Convert to a document term matrix (rows are documents, columns are words)
+require(tm)
+dtm <- DocumentTermMatrix(d, control = list(weighting = weightTfIdf))
+dtm2 <- DocumentTermMatrix(d)
+
+# Collect the frequency, the tf idf, the positivity, and the negativity into a single dataframe
+words <- data.frame(counts = colSums(as.matrix(dtm2)))
+words$tfidf <- colSums(as.matrix(dtm))
+words$positivity <- colSums(as.matrix(dtm2)[dr$stars > 3,]) / dim(dtm)[1]
+words$negativity <- colSums(as.matrix(dtm2)[dr$stars < 4,]) / dim(dtm)[1]
+words$sentiment <- words$positivity / words$negativity
+words$words <- row.names(words)
+row.names(words) <- 1:dim(dtm)[2]
+
+head(words[order(words$sentiment, decreasing = FALSE) & words$counts > 10,], 10)
+
+# Sanity checking
+words['iphone',]
+colSums(as.matrix(dtm))['iphone']
+
+loc <- '/Users/josiahdavis/Documents/DraftBlog/yelp_dataset_challenge_academic_dataset/'
