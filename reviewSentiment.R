fcffbb63d0d9b81c24bbefcd45c9f08db6eb583b
josiahdavis <josiah.j.davis@gmail.com>
2015-08-27 07:19:07 -0700

    removed unnecessary code

--- reviewSentiment.R
+++ reviewSentiment.R
@@ -1,38 +1,25 @@
 # ANALYSIS OF YELP DATA
 # This script quantifies the sentiments associated with each review.
-
 library(syuzhet)
 
 
-# Read in data
+# Read in and subset data
 loc <- '/Users/josiahdavis/Documents/GitHub/earl/'
 dr <- read.csv(paste(loc, 'yelp_review.csv', sep=""))
+d <- dr[,c("text", "stars", "votes_cool", "votes_funny", "votes_useful")]
 
 
 # For each review, calculate various sentiment related metrics
-d <- dr[,c("text", "stars")]
-d$length <- NULL
-d$meanSent <- NULL
-d$varSent <- NULL
-d$negCount <- NULL
-d$neutCount <- NULL
-d$posCount <- NULL
-
-for (i in 1:nrow(d)){
-  d$length[i] = length(get_sentences(as.character(d$text)))
-}
-
-sentances <- function(x) get_sentences(as.character(x))
-
 d$sentances <- unlist(lapply(d$text, function(x) length(get_sentences(as.character(x)))))
+d$totalSentBING <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)), method = "bing"))))
+d$totalSentNRC <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)), method = "afinn"))))
+d$totalSentAFINN <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)), method = "nrc"))))
+d$totalSent <- rowMeans(d[,c("totalSentBING", "totalSentNRC", "totalSentAFINN")])
 d$meanSent <- unlist(lapply(d$text, function(x) mean(get_sentiment(get_sentences(as.character(x)), method = "bing"))))
 d$varSent <- unlist(lapply(d$text, function(x) var(get_sentiment(get_sentences(as.character(x)),   method = "bing"))))
 d$negCount <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)),  method = "bing") < 0)))
 d$posCount <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)),  method = "bing") > 0)))
 d$neutCount <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)), method = "bing") == 0)))
 
+# Write out dataframe
 write.csv(d, paste(loc, 'sentiment.csv', sep=""), row.names=FALSE)
-
-review<- get_sentences(as.character(d$text[1]))
-length(review)
-sent <- sum(get_sentiment(review, method="bing") > 0)
