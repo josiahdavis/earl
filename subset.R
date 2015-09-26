9ad3d47790ef65bfd501c6003f15fd92e6ea1cef
josiahdavis <josiah.j.davis@gmail.com>
2015-09-26 10:03:19 -0700

    added business attributes of interest

--- subset.R
+++ subset.R
@@ -1,12 +1,9 @@
 # Create subset of yelp data for analysis
 
-loc <- '/Users/josiahdavis/Documents/DraftBlog/yelp_dataset_challenge_academic_dataset/'
-
 # Read in business attributes
+loc <- '/Users/josiahdavis/Documents/DraftBlog/yelp_dataset_challenge_academic_dataset/'
 db <- read.csv(paste(loc, 'yelp_academic_dataset_business.csv', sep=""))
 
-# Which states are most representative?
-summary(db$state)
 
 # Which business categories are their? 
 #     Arts & Entertainment,Performing Arts
@@ -26,10 +23,14 @@ dim(db)
 # Read in review text
 dr <- read.csv(paste(loc, 'yelp_academic_dataset_review.csv', sep=""), sep=",")
 
-# Subset to only include those for Verizon Wireless
+# Subset reviews to only include those for one of three banks
 dr <- dr[dr$business_id %in% db$business_id, ]
+
+# Add business information into the main dataframe
+d <- merge(dr, db[,c("business_id", "name", "review_count", "stars", "state", "full_address")],
+      by = "business_id", all.x = TRUE, all.y = FALSE)
 
 # Write subset data
 newLoc <- '/Users/josiahdavis/Documents/GitHub/earl/'
-write.csv(db, paste(newLoc, 'yelp_business.csv', sep=""), row.names=FALSE)
-write.csv(dr, paste(newLoc, 'yelp_review.csv', sep=""), row.names=FALSE)
+# write.csv(db, paste(newLoc, 'yelp_business.csv', sep=""), row.names=FALSE)
+write.csv(d, paste(newLoc, 'yelp_review.csv', sep=""), row.names=FALSE)
