74b3928f46a8bf66ce9623f812bba052723b5352
josiahdavis <josiah.j.davis@gmail.com>
2015-09-28 06:03:56 -0700

    updated sentiment csv file with banking data

--- reviewSentiment.R
+++ reviewSentiment.R
@@ -1,15 +1,15 @@
 # ANALYSIS OF YELP DATA
+
+# Clear working space
+rm(list = ls())
+gc()
+
 # This script quantifies the sentiments associated with each review.
 library(syuzhet)
 
-
 # Read in, subset, and merge data
 loc <- '/Users/josiahdavis/Documents/GitHub/earl/'
-db <- read.csv(paste(loc, 'yelp_business.csv', sep=""))
-db <- db[,c("business_id","name", "state", "city")]
-dr <- read.csv(paste(loc, 'yelp_review.csv', sep=""))
-dr <- dr[,c("text", "stars", "votes_cool", "votes_funny", "votes_useful", "business_id")]
-d <- merge(dr,db,by="business_id")
+d <- read.csv(paste(loc, 'yelp_review.csv', sep=""))
 
 # For each review, calculate various sentiment related metrics
 d$sentances <- unlist(lapply(d$text, function(x) length(get_sentences(as.character(x)))))
