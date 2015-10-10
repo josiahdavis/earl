970dfc16daa8ad7c2a22dc2da300ab4d3191c057
josiahdavis <josiah.j.davis@gmail.com>
2015-10-10 02:39:26 -0700

    Added word based positive and negative talies

--- reviewSentiment.R
+++ reviewSentiment.R
@@ -10,19 +10,14 @@ library(plyr)
 
 # Read in, subset, and merge data
 loc <- '/Users/josiahdavis/Documents/GitHub/earl/'
-d <- read.csv(paste(loc, 'yelp_review.csv', sep=""))
+d <- read.csv(paste(loc, 'yelp_review_Banking.csv', sep=""))
 
-# For each review, calculate various sentiment related metrics
-d$sentances <- unlist(lapply(d$text, function(x) length(get_sentences(as.character(x)))))
-d$totalSentBING <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)), method = "bing"))))
-d$totalSentNRC <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)), method = "afinn"))))
-d$totalSentAFINN <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)), method = "nrc"))))
-d$totalSent <- rowMeans(d[,c("totalSentBING", "totalSentNRC", "totalSentAFINN")])
-d$meanSent <- unlist(lapply(d$text, function(x) mean(get_sentiment(get_sentences(as.character(x)), method = "bing"))))
-d$varSent <- unlist(lapply(d$text, function(x) var(get_sentiment(get_sentences(as.character(x)),   method = "bing"))))
-d$negCount <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)),  method = "bing") < 0)))
-d$posCount <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)),  method = "bing") > 0)))
-d$neutCount <- unlist(lapply(d$text, function(x) sum(get_sentiment(get_sentences(as.character(x)), method = "bing") == 0)))
+# For each review, calculate the count of negative and positive sentiments
+ds <- data.frame(t(apply(as.matrix(d$text), 1, function(x) colSums(
+  get_nrc_sentiment(get_sentences(as.character(x)))[c("negative", "positive")])
+  )))
+
+ds <- cbind(d, ds)
 
 # Write out dataframe
-write.csv(d, paste(loc, 'sentiment.csv', sep=""), row.names=FALSE)
+write.csv(ds, paste(loc, 'yelp_review_Banking.csv', sep=""), row.names=FALSE)
