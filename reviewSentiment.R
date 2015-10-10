6e19e1f810a626c5366b007e325ff087e111ad19
josiahdavis <josiah.j.davis@gmail.com>
2015-10-10 16:13:55 -0700

    Added positivity and negativity review metrics based on ratio to total words

--- reviewSentiment.R
+++ reviewSentiment.R
@@ -17,7 +17,12 @@ ds <- data.frame(t(apply(as.matrix(d$text), 1, function(x) colSums(
   get_nrc_sentiment(get_sentences(as.character(x)))[c("negative", "positive")])
   )))
 
+# Add back into the dataframe
 ds <- cbind(d, ds)
+
+# Create two helper variables
+ds$positivity <- ds$positive / ds$wordsLength
+ds$negativity <- ds$negative / ds$wordsLength
 
 # Write out dataframe
 write.csv(ds, paste(loc, 'yelp_review_Banking.csv', sep=""), row.names=FALSE)
